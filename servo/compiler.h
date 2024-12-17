5ee71a1e98b3166bfbdd84e1a837e7e53f9a8dd6
Wylan <wylanshoemaker@gmail.com>
2024-12-17 13:04:45 -0700

    TokenTypes and Tokens

---
+++ servo/compiler.h
@@ -0,0 +1,10 @@
+//
+// Created by wylan on 12/17/24.
+//
+
+#ifndef COMPILER_H
+#define COMPILER_H
+
+void compile(const char *source);
+
+#endif //COMPILER_H
---
+++ servo/scanner.h
@@ -0,0 +1,44 @@
+//
+// Created by wylan on 12/17/24.
+//
+
+#ifndef SCANNER_H
+#define SCANNER_H
+
+typedef enum {
+    TOKEN_LEFT_PAREN, TOKEN_RIGHT_PAREN,
+    TOKEN_LEFT_BRACE, TOKEN_RIGHT_BRACE,
+    TOKEN_LEFT_BRACKET, TOKEN_RIGHT_BRACKET,
+    TOKEN_COMMA, TOKEN_DOT, TOKEN_MINUS, TOKEN_PLUS,
+    TOKEN_SEMICOLON, TOKEN_SLASH, TOKEN_STAR, TOKEN_MOD, TOKEN_POW,
+
+    // One or two character tokens.
+    TOKEN_BANG, TOKEN_BANG_EQUAL,
+    TOKEN_EQUAL, TOKEN_EQUAL_EQUAL,
+    TOKEN_GREATER, TOKEN_GREATER_EQUAL,
+    TOKEN_LESS, TOKEN_LESS_EQUAL,
+    TOKEN_RIGHT_POINTER, TOKEN_LEFT_POINTER,
+
+    // literals
+    TOKEN_IDENTIFIER, TOKEN_STRING, TOKEN_NUMBER,
+
+    // keywords
+    TOKEN_AND, TOKEN_CLASS, TOKEN_ELSE, TOKEN_FALSE,
+    TOKEN_FOR, TOKEN_FUNCTION, TOKEN_IF, TOKEN_NULL, TOKEN_OR,
+    TOKEN_PRINT, TOKEN_RETURN, TOKEN_SUPER, TOKEN_THIS,
+    TOKEN_TRUE, TOKEN_VAR, TOKEN_WHILE,
+
+    TOKEN_ERROR, TOKEN_EOF
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    const char *start;
+    int length;
+    int line;
+} Token;
+
+void initScanner(const char *source);
+Token scanToken();
+
+#endif //SCANNER_H
